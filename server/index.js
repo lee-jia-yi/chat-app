e1cdecdfea64b6667b2ce010ac52d70ae9a19437
Lee Jia Yi <89006263+lee-jia-yi@users.noreply.github.com>
2023-07-14 00:16:17 +0800

    clean up

--- server/index.js
+++ server/index.js
@@ -66,65 +66,6 @@ io.on('connection', (socket) => {
             socket.to(sendUserSocket).emit("receive-msg", data);
         }
     });
-
-    // // expects event sent from FE
-    // socket.on('join', ({ name, room }, callback) => {
-    //     const { error, user } = addUser({ id: socket.id, name, room });
-    //     if (error) return callback(error);
-
-    //     // admin generated messages; Emit event from BE to FE
-    //     socket.emit('message', {
-    //         user: 'admin',
-    //         text: `You joined ${user.room}`,
-    //         isAdmin: true
-    //     });
-
-    //     // built in func that sends message to everyone besides that user
-    //     socket.broadcast.to(user.room).emit('message', {
-    //         user: 'admin',
-    //         text: `${user.name} has joined the room`,
-    //         isAdmin: true
-    //     });
-
-    //     socket.join(user.room); // built in function that joins a room
-
-    //     io.to(user.room).emit('roomData', {
-    //         room: user.room,
-    //         users: getUsersInRoom(user.room)
-    //     });
-
-    //     callback();
-    // });
-
-
-    // // user generated messages; expect the event from FE
-    // socket.on('sendMessage', (message, callback) => {
-    //     const user = getUser(socket.id);
-
-    //     io.to(user.room).emit('message', {
-    //         user: user.name,
-    //         text: message,
-    //         isAdmin: false
-    //     });
-
-    //     io.to(user.room).emit('roomData', {
-    //         room: user.room,
-    //         users: getUsersInRoom(user.room)
-    //     });
-
-    //     callback();
-    // });
-
-    // socket.on('disconnect', () => {
-    //     const user = removeUser(socket.id);
-    //     if (user) {
-    //         io.to(user.room).emit('message', {
-    //             user: 'admin',
-    //             text: `${user.name} has left`,
-    //             isAdmin: true
-    //         });
-    //     }
-    // });
 });
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
